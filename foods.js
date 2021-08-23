84420951a720e3b55ee5db44aa474f628e31a41d
Cole Fortner <colefortner@gmail.com>
2021-08-22 21:29:09 -0400

    Add additional meals and calculate full day food data

--- foods.js
+++ foods.js
@@ -22,20 +22,16 @@ class Food {
     this.fiber = fiber;
     this.totSugars = sugars;
     this.protein = protein;
-    // this.vitA = vitA;
-    // this.vitC = vitC;
-    // this.ca = ca;
-    // this.fe = fe;
   }
 }
 
-let trueStoryChicken = new Food(56, 80, 2, 0, 45, 110, 0, 0, 0, 15);
+const trueStoryChicken = new Food(56, 80, 2, 0, 45, 110, 0, 0, 0, 15);
 trueStoryChicken.vitA = 0;
 trueStoryChicken.vitC = 0;
 trueStoryChicken.ca = 0;
 trueStoryChicken.fe = 2;
 
-let horizonOrgAmerican = new Food(19, 60, 4, 2.5, 10, 240, 1, 0, 1, 4);
+const horizonOrgAmerican = new Food(19, 60, 4, 2.5, 10, 240, 1, 0, 1, 4);
 
 horizonOrgAmerican.vitD = 0;
 horizonOrgAmerican.ca = 10;
@@ -43,14 +39,14 @@ horizonOrgAmerican.fe = 0;
 horizonOrgAmerican.kMg = 0;
 horizonOrgAmerican.addedSugarsG = 1;
 
-let davesEnglishMuff = new Food(62, 140, 1.5, 0, 0, 300, 26, 3, 2, 6);
+const davesEnglishMuff = new Food(62, 140, 1.5, 0, 0, 300, 26, 3, 2, 6);
 
 davesEnglishMuff.vitD = 0;
 davesEnglishMuff.ca = 0;
 davesEnglishMuff.fe = 6;
 davesEnglishMuff.kMg = 110;
 
-let kashiChocolate = new Food(61, 210, 1.5, 0, 0, 5, 50, 7, 9, 5);
+const kashiChocolate = new Food(61, 210, 1.5, 0, 0, 5, 50, 7, 9, 5);
 kashiChocolate.soluableFiberG = 1;
 kashiChocolate.insoluableFiberG = 6;
 kashiChocolate.addedSugarsG = 9;
@@ -63,7 +59,7 @@ kashiChocolate.niacin = 25;
 kashiChocolate.magnesium = 15;
 kashiChocolate.zinc = 10;
 
-let almondBreezeUnsweetendVanilla = new Food(240, 30, 2.5, 0, 0, 170, 1, 0, 0, 1);
+const almondBreezeUnsweetendVanilla = new Food(240, 30, 2.5, 0, 0, 170, 1, 0, 0, 1);
 almondBreezeUnsweetendVanilla.vitD = 25;
 almondBreezeUnsweetendVanilla.ca = 35;
 almondBreezeUnsweetendVanilla.fe = 4;
@@ -73,33 +69,31 @@ almondBreezeUnsweetendVanilla.vitE = 50;
 almondBreezeUnsweetendVanilla.phosphorus = 2;
 almondBreezeUnsweetendVanilla.magnesium = 4;
 
-let franksBuff = new Food(15, 0, 0, 0, 0, 420, 0, 0, 0, 0);
+const franksBuff = new Food(15, 0, 0, 0, 0, 420, 0, 0, 0, 0);
 
-let greenwiseSpinach = new Food(142, 35, 0, 0, 0, 110, 5, 3, 0, 4);
+const greenwiseSpinach = new Food(142, 35, 0, 0, 0, 110, 5, 3, 0, 4);
 greenwiseSpinach.vitD = 0;
 greenwiseSpinach.ca = 10;
 greenwiseSpinach.fe = 20;
 greenwiseSpinach.kMg = 790;
 
-let greenwiseSunflowerSeeds = new Food(30, 200, 17, 1, 0, 170, 6, 5, 0, 5);
+const greenwiseSunflowerSeeds = new Food(30, 200, 17, 1, 0, 170, 6, 5, 0, 5);
 greenwiseSunflowerSeeds.vitD = 0;
 greenwiseSunflowerSeeds.ca = 2;
 greenwiseSunflowerSeeds.fe = 6;
 // greenwiseSpinach.kMg = 790;??????????? 4%
 
-let tomatoUSDA = new Food(123, 22, 0, 0, 0, 6, 5, 2, 3, 1);
+const tomatoUSDA = new Food(123, 22, 0, 0, 0, 6, 5, 2, 3, 1);
 tomatoUSDA.vitD = 0;
 tomatoUSDA.ca = 1;
 tomatoUSDA.fe = 2;
 tomatoUSDA.kMg = 291;
 
-let bananaUSDA = new Food(118, 105, 0, 0, 0, 1, 27, 3, 14, 1);
+const bananaUSDA = new Food(118, 105, 0, 0, 0, 1, 27, 3, 14, 1);
 bananaUSDA.vitD = 0;
 bananaUSDA.ca = 6;
 bananaUSDA.fe = 1;
 bananaUSDA.kMg = 517;
-
-// console.log(trueStoryChicken)
 
 function servingCalculation(food, serving){
   let factor = serving/food.servingSizeG
@@ -116,8 +110,6 @@ function mealCalculation(foodArr){
   let meal = { }
   for(let food of foodArr){
     for(let key in food){
-      console.log(typeof food[key])
-      // console.log(key)
       if (!meal[key]){
         meal[key] = food[key]
       } else {
@@ -127,19 +119,32 @@ function mealCalculation(foodArr){
   }
   return meal
 }
-console.log(mealCalculation(
-                            [
-                              servingCalculation(trueStoryChicken, 56), 
-                              horizonOrgAmerican, 
-                              franksBuff, 
-                              servingCalculation(tomatoUSDA, 30),
-                              davesEnglishMuff,
-                              servingCalculation(greenwiseSpinach, 10)
-                            ]
-                          )
-                        )
-let calculatedTomato = servingCalculation(tomatoUSDA, 30);
-console.log(calculatedTomato);
-let calculatedSpinach = servingCalculation(greenwiseSpinach, 10);
-console.log(calculatedSpinach)
+
+const breakfast = [
+  servingCalculation(trueStoryChicken, 56), 
+  horizonOrgAmerican, 
+  franksBuff, 
+  servingCalculation(tomatoUSDA, 30),
+  davesEnglishMuff,
+  servingCalculation(greenwiseSpinach, 10)
+]
+
+const lunch = [
+  servingCalculation(trueStoryChicken, 64), 
+  horizonOrgAmerican, 
+  franksBuff, 
+  servingCalculation(tomatoUSDA, 40),
+  davesEnglishMuff,
+  servingCalculation(greenwiseSpinach, 10),
+  kashiChocolate,
+  almondBreezeUnsweetendVanilla
+]
+
+const snack = [
+  servingCalculation(bananaUSDA, 89),
+  servingCalculation(greenwiseSunflowerSeeds, 15)
+]
+
+console.log(mealCalculation([mealCalculation(breakfast), mealCalculation(lunch), mealCalculation(snack)]))
+
 
